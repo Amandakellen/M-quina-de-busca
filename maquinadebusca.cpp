ad523ce9b3b38de9a7b1ab2cfb16fc1695e880bf
danielasampaio <51174889+danielasampaio@users.noreply.github.com>
2019-06-02 13:15:11 -0300

    Mudanças no construtor em "lerarquivos" e implementação da "criando map" e da "redimenciona"
    Verificamos que seria necessário a criação de um método para redimensionar, pois quando um arquivo excedia a capacidade da string, dava erro e um método que estamos usando antes para redimensionar, não estava correto. Logo, tivemos que mudar também o construtor para que ele inicializasse as variáveis t1, t2, t3 que são responsáveis por armazenar a quantidade de elementos de cada string.
    Em "lerarquivos" , chamados o método redimensiona caso a capacidade seja excedida.
    Na criando map, por enquanto estamos testando apenas com um arquivo, para depois implementar com os outros arquivos. Nessa função, criamos um índice invertido.

--- maquinadebusca.cpp
+++ maquinadebusca.cpp
@@ -2,20 +2,35 @@
 #include <string.h>
 #include <fstream>
 #include <ctype.h>
-
+#include <map>
 #include "maquinadebusca.h"
 using std::cout;
 using std::endl;
 using std::string;
+using std::multimap;
+using std::pair;
 
 maquinadebusca::maquinadebusca()
 {
 	c1=100;
 	c2=100;
 	c3=100;
+	t1=1;
+	t2=1;
+	t3=1;
 	arq1=new string[c1];
 	arq2=new string[c2];
 	arq3=new string[c3];
+}
+void maquinadebusca::redimensiona(int n)
+{
+	string *aux=new string[n];
+      for(int j=0;j<t1;j++){
+        aux[j]=arq1[j];
+      }
+			delete []arq1;
+			arq1=aux;
+			c1=n;
 }
 void maquinadebusca::lerarquivos()
 {
@@ -25,56 +40,47 @@ void maquinadebusca::lerarquivos()
     in.open("d1.txt");
     char auxiliar;
 if(in.is_open()){
-  while((auxiliar=in.get())!=EOF){
-    if(i>c1){		
-      string *aux=new string[c1*2];
-      for(int j=0;i<(c1*2);j++){
-        aux[j]=arq1[j];
-      }
-			delete []arq1;
-			c1=c1*2;
-			arq1=aux;
-    }else{
-      if( isalpha(auxiliar)) {//verifica se é uma letra
-       if(isupper(auxiliar)){//verifica se é letra maiscula
-			auxiliar=tolower(auxiliar);
-            arq1[indice]=auxiliar;
-       }else{
-         arq1[indice]=auxiliar;
-        }
-      }else{
-        if(isdigit(auxiliar)){//verifica se é numero
-	      arq1[indice]=auxiliar;
-        }else{
-        	if(auxiliar==32){//verifica se é espaço
-			 arq1[indice]=auxiliar;	
-			}
-			else{
-             if( auxiliar==46){//verifica se é .
-       	     arq1[indice]=auxiliar;
-             }
-             if( auxiliar==33){//verifica se é !
-       	     arq1[indice]=auxiliar;
-             }
-             if( auxiliar==44){//verifica se é ,
-       	     arq1[indice]=auxiliar;
-             }
-             if( auxiliar==58){//verifica se é :
-       	     arq1[indice]=auxiliar;
-             }
-             if( auxiliar==59){//verifica se é ;
-       	     arq1[indice]=auxiliar;
-             }
-             if( auxiliar==63){//verifica se é ?
-       	     arq1[indice]=auxiliar;
-             }
-			 
-			}
+while((auxiliar=in.get())!=EOF){
+ if(t1==c1){	
+ redimensiona(2*c1);
+ }else{
+  if( isalpha(auxiliar)) {//verifica se Ã© uma letra
+    if(isupper(auxiliar)){//verifica se Ã© letra maiscula
+	 auxiliar=tolower(auxiliar);
+     arq1[indice]=auxiliar;
+    }else{arq1[indice]=auxiliar;}
+  }else{
+    if(isdigit(auxiliar)){//verifica se Ã© numero
+     arq1[indice]=auxiliar;
+     }else{
+   	  if(auxiliar==32){//verifica se Ã© espaÃ§o
+	   arq1[indice]=auxiliar;}else{
+             if(ispunct(auxiliar) ){//verifica se Ã© um ponto
+              if(auxiliar==33){arq1[indice]=auxiliar;}
+              else{
+                 if(auxiliar==44){arq1[indice]=auxiliar;}
+                 else{ 
+	              if(auxiliar==58){arq1[indice]=auxiliar;}
+                  else{
+                    if(auxiliar==46){arq1[indice]=auxiliar;}
+                    else{
+                      if(auxiliar==59){arq1[indice]=auxiliar;}
+                      else{
+	                    if(auxiliar==63){arq1[indice]=auxiliar;}
+                        else{indice=indice-1;}
+	                  }
+                    }
+                  }
+                }
+               }
+           }
+       }
           
          }
       }
     i++;
     indice++;
+    t1++;
 	}
 		
   }
@@ -86,56 +92,49 @@ i=0;
 indice=0;
 if(in.is_open()){
 while((auxiliar=in.get())!=EOF){
- if(i>c2){		
-      string *aux=new string[c2*2];
-      for(int j=0;i<(c2*2);j++){
-        aux[j]=arq1[j];
-      }
-			delete []arq2;
-			c2=c2*2;
-			arq2=aux;
+ if(t2==c2){	
+ redimensiona(2*c2);
  }else{
-  if( isalpha(auxiliar)) {//verifica se é uma letra
-    if(isupper(auxiliar)){//verifica se é letra maiscula
+  if( isalpha(auxiliar)) {//verifica se Ã© uma letra
+    if(isupper(auxiliar)){//verifica se Ã© letra maiscula
 	 auxiliar=tolower(auxiliar);
      arq2[indice]=auxiliar;
-    }else{
+    }else{arq2[indice]=auxiliar;}
+  }else{
+    if(isdigit(auxiliar)){//verifica se Ã© numero
      arq2[indice]=auxiliar;
-     }
-      }else{
-        if(isdigit(auxiliar)){//verifica se é numero
-	      arq2[indice]=auxiliar;
-        }else{
-        	if(auxiliar==32){//verifica se é espaço
-			 arq2[indice]=auxiliar;	
-			}
-			else{
-             if( auxiliar==46){//verifica se é .
-       	     arq2[indice]=auxiliar;
-             }
-             if( auxiliar==33){//verifica se é !
-       	     arq2[indice]=auxiliar;
-             }
-             if( auxiliar==44){//verifica se é ,
-       	     arq2[indice]=auxiliar;
-             }
-             if( auxiliar==58){//verifica se é :
-       	     arq2[indice]=auxiliar;
-             }
-             if( auxiliar==59){//verifica se é ;
-       	     arq2[indice]=auxiliar;
-             }
-             if( auxiliar==63){//verifica se é ?
-       	     arq2[indice]=auxiliar;
-             }
-			 
-			}
+     }else{
+   	  if(auxiliar==32){//verifica se Ã© espaÃ§o
+	   arq2[indice]=auxiliar;}else{
+             if(ispunct(auxiliar) ){//verifica se Ã© um ponto
+              if(auxiliar==33){arq2[indice]=auxiliar;}
+              else{
+                 if(auxiliar==44){arq2[indice]=auxiliar;}
+                 else{ 
+	              if(auxiliar==58){arq2[indice]=auxiliar;}
+                  else{
+                    if(auxiliar==46){arq2[indice]=auxiliar;}
+                    else{
+                      if(auxiliar==59){arq2[indice]=auxiliar;}
+                      else{
+	                    if(auxiliar==63){arq2[indice]=auxiliar;}
+                        else{indice=indice-1;}
+	                  }
+                    }
+                  }
+                }
+               }
+           }
+       }
+          
          }
       }
     i++;
     indice++;
+    t2++;
 	}
- }
+		
+  }
 }
 in.close();
 
@@ -144,58 +143,80 @@ i=0;
 indice=0;
 if(in.is_open()){
 while((auxiliar=in.get())!=EOF){
- if(i>c3){		
-      string *aux=new string[c3*2];
-      for(int j=0;i<(c3*2);j++){
-        aux[j]=arq3[j];
-      }
-			delete []arq3;
-			c3=c3*2;
-			arq3=aux;
+ if(t3==c3){	
+ redimensiona(2*c3);
  }else{
-  if( isalpha(auxiliar)) {//verifica se é uma letra
-    if(isupper(auxiliar)){//verifica se é letra maiscula
+  if( isalpha(auxiliar)) {//verifica se Ã© uma letra
+    if(isupper(auxiliar)){//verifica se Ã© letra maiscula
 	 auxiliar=tolower(auxiliar);
      arq3[indice]=auxiliar;
-    }else{
+    }else{arq3[indice]=auxiliar;}
+  }else{
+    if(isdigit(auxiliar)){//verifica se Ã© numero
      arq3[indice]=auxiliar;
-     }
-      }else{
-        if(isdigit(auxiliar)){//verifica se é numero
-	      arq3[indice]=auxiliar;
-        }else{
-        	if(auxiliar==32){//verifica se é espaço
-			 arq3[indice]=auxiliar;	
-			}
-			else{
-             if( auxiliar==46){//verifica se é .
-       	     arq3[indice]=auxiliar;
-             }
-             if( auxiliar==33){//verifica se é !
-       	     arq3[indice]=auxiliar;
-             }
-             if( auxiliar==44){//verifica se é ,
-       	     arq3[indice]=auxiliar;
-             }
-             if( auxiliar==58){//verifica se é :
-       	     arq3[indice]=auxiliar;
-             }
-             if( auxiliar==59){//verifica se é ;
-       	     arq3[indice]=auxiliar;
-             }
-             if( auxiliar==63){//verifica se é ?
-       	     arq3[indice]=auxiliar;
-             }
-			 
-			}
+     }else{
+   	  if(auxiliar==32){//verifica se Ã© espaÃ§o
+	   arq3[indice]=auxiliar;}else{
+             if(ispunct(auxiliar) ){//verifica se Ã© um ponto
+              if(auxiliar==33){arq3[indice]=auxiliar;}
+              else{
+                 if(auxiliar==44){arq3[indice]=auxiliar;}
+                 else{ 
+	              if(auxiliar==58){arq3[indice]=auxiliar;}
+                  else{
+                    if(auxiliar==46){arq3[indice]=auxiliar;}
+                    else{
+                      if(auxiliar==59){arq3[indice]=auxiliar;}
+                      else{
+	                    if(auxiliar==63){arq3[indice]=auxiliar;}
+                        else{indice=indice-1;}
+	                  }
+                    }
+                  }
+                }
+               }
+           }
+       }
+          
          }
       }
     i++;
     indice++;
+    t3++;
 	}
- }
+		
+  }
 }
 in.close();
 
+}
+void maquinadebusca:: criandomap()
+{
+string aux;
+string aux3;
+for(int i=0;i<t1;i++){
+ aux=arq1[i];
+ 
+   if(aux!=" " && aux!=","&& aux!="." && aux!="!" && aux!=":" && aux!=";" && aux!="?")
+    {
+      if(i==0){aux3=aux;}
+	   else{aux3=aux3+aux;}
+        aux="";   
+    }
+    else
+    {
+    	
+	   if(aux=="." &&aux=="," && aux=="." && aux=="!" && aux==":" && aux==";" && aux=="?" )
+	   {
+	   	indice_invertido.insert(pair<string,string>(aux,"arq1"));
+	   }
+	   else{
+		   indice_invertido.insert(pair<string,string>(aux3,"arq1"));
+		   aux3="";
+	   }
+	}
+    
+
+    }
 
 }
