4f1db58295bcb4508e378430b488d242515c6ad1
danielasampaio <51174889+danielasampaio@users.noreply.github.com>
2019-06-02 13:01:03 -0300

    Acréscimo dos métodos
    Acrescentamos o método "criando map", que salva os dados dos arquivos para criar um índice invertido, para isso foi criado um multimap chamado indice_invertido, para salvar esse índice. O método "redimenciona" para redimensionar os tamanhos das strings arq1, arq2 e arq3, que são responsáveis por armazenar os dados dos arquivos. Também foi criado as variáveis t1, t2, t3 que são responsáveis por armazenas a quantidade de dados das strings arq1, arq2, arq3, respectivamente.

--- maquinadebusca.h
+++ maquinadebusca.h
@@ -9,11 +9,15 @@ class maquinadebusca{
 	public:
     maquinadebusca();
 	void lerarquivos();	
-		
+	void criandomap();	
     private:
- 	string * arq1;
- 	string * arq2;
+    multimap <string,string> indice_invertido;// usando multimap podem haver chaves iguais com indices diferentes;	
+ 	string *arq1;
+ 	string *arq2;
  	string *arq3;
- 	int c1,c2,c3;		
+ 	int c1,c2,c3;
+ 	int t1,t2,t3;
+ 	void redimensiona(int n);
+			
 };
 #endif
