7dd8acba10f96b3de0b06aea80b8b11080bae41a
Amanda Kellen <49374780+Amandakellen@users.noreply.github.com>
2019-06-10 14:06:21 -0300

    acrescimo de funções e variáveis
    acresicmo de variaveis  arq1_tf,arq2_tf,arq3_tf  que recebem a frequencia dos documentos d1,d2 e d3 respectivamente.Além disso acrescentamos arq1_,arq2_,arq3_ que salvam os dados de cada arquivo e a função void tf() que faz o calculo da frequencia de cada palavra em cada arquivo

--- maquinadebusca.h
+++ maquinadebusca.h
@@ -3,16 +3,26 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 using std::string;
 using std::multimap;
+using std::map;
 class maquinadebusca{
 
 	public:
     maquinadebusca();
 	void lerarquivos();	
 	void criandomap();	
+	void tf();
     private:
     multimap <string,string> indice_invertido;// usando multimap podem haver chaves iguais com indices diferentes;	
+    multimap <string,double>idf_;
+    map <string,int>arq1_tf;
+    map <string,int>arq2_tf;
+    map <string,int>arq3_tf;
+    multimap <string,string> arq1_;
+    multimap <string,string> arq2_;
+    multimap <string,string> arq3_;
  	string *arq1;
  	string *arq2;
  	string *arq3;
