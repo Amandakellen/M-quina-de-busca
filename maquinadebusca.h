d965e160570836fc790eb04f62b502deb3a7fc0d
SilvioFJr <51271373+SilvioFJr@users.noreply.github.com>
2019-06-12 19:34:35 -0300

    Acrescimo de metodos
    Acrescimo das variáveis wd1,wd2,wd3 que armazenam as coordenadas dos arquivos e também do método void coordenada_do_documento() que faz o calculo dessas coordenadas.

--- maquinadebusca.h
+++ maquinadebusca.h
@@ -10,19 +10,24 @@ using std::map;
 class maquinadebusca{
 
 	public:
-    maquinadebusca();
-	void lerarquivos();	
-	void criandomap();	
-	void tf();
+    maquinadebusca();//construtor
+	void lerarquivos();	//leitura dos arquivos
+	void criandomap();	//cria map com dados de cada arquivo(cria indice invertido também)
+	void tf();//cria o tf de cada arquivo
+	void coordenada_do_documento();//cria a coordenada de cada arquivo
+
     private:
-    multimap <string,string> indice_invertido;// usando multimap podem haver chaves iguais com indices diferentes;	
+    multimap <string,string>indice_invertido;// usando multimap podem haver chaves iguais com indices diferentes;	
     multimap <string,double>idf_;
-    map <string,int>arq1_tf;
-    map <string,int>arq2_tf;
-    map <string,int>arq3_tf;
-    multimap <string,string> arq1_;
-    multimap <string,string> arq2_;
-    multimap <string,string> arq3_;
+    map <string,int>arq1_tf;//frequencia do arquivo1 
+    map <string,int>arq2_tf;//frequencia do arquivo2 
+    map <string,int>arq3_tf;//frequencia do arquivo3
+    map <string,double> wd1;//coordenada do arquivo1 
+	map <string,double> wd2;//coordenada do arquivo2 
+	map <string,double> wd3;//coordenada do arquivo3 
+    multimap <string,string> arq1_;//dados do arquivo 1
+    multimap <string,string> arq2_;//dados do arquivo 2
+    multimap <string,string> arq3_;//dados do arquivo 3
  	string *arq1;
  	string *arq2;
  	string *arq3;
@@ -31,4 +36,5 @@ class maquinadebusca{
  	void redimensiona(int n);
 			
 };
+
 #endif
